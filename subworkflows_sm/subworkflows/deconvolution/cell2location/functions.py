46d3039436f795e32a8f0f298bab5c24736a7d08
abderahim <abderahim@cbib-Precision-T1500>
2024-06-07 14:34:50 +0200

    d

--- subworkflows_sm/subworkflows/deconvolution/cell2location/functions.py
+++ subworkflows_sm/subworkflows/deconvolution/cell2location/functions.py
@@ -33,7 +33,7 @@ def build_cell2location_model(sc_input):
     print(f"Building cell2location model with {'GPU' if params['gpu'] else 'CPU'}...")
     
     command = [
-        "bash", "-c", f"source activate cell2loc_env && python build_model.py -a {params['annot']} {sample_id_arg} {epochs} {args} -o {output_dir} {sc_input} {cuda_device}"
+        "bash", "-c", f"source activate cell2loc_env && python build_model.py {sc_input} {cuda_device} -a {params['annot']} {sample_id_arg} {epochs} {args} -o {output_dir}"
     ]
     print(command)
     subprocess.run(command, check=True)
