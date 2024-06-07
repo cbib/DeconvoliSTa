3528cd476ad23534f6ddb1824e5925f634af3661
abderahim <abderahim@cbib-Precision-T1500>
2024-06-07 14:27:28 +0200

    d

--- subworkflows_sm/subworkflows/deconvolution/cell2location/functions.py
+++ subworkflows_sm/subworkflows/deconvolution/cell2location/functions.py
@@ -35,7 +35,7 @@ def build_cell2location_model(sc_input):
     command = [
         "bash", "-c", f"""
         source activate cell2loc_env &&
-        python build_model.py \\ -a {params['annot']} {sample_id_arg} {epochs} {args} -o {output_dir}
+        python build_model.py -a {params['annot']} {sample_id_arg} {epochs} {args} -o {output_dir}
             {sc_input} {cuda_device} 
         """
     ]
