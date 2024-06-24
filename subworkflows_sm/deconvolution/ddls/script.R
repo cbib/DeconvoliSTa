b0102297818ea650082769c0d92380be4161209d
abderahim <Vous@exemple.com>
2024-06-24 17:38:27 +0200

    1st version working but with an issue when using sudo

--- subworkflows_sm/deconvolution/ddls/script.R
+++ subworkflows_sm/deconvolution/ddls/script.R
@@ -1,67 +1,77 @@
-# if (!requireNamespace("SpatialDDLSdata", quietly = TRUE)) {
-#     remotes::install_github('diegommcc/SpatialDDLSdata')
-# }
-# library("SpatialDDLSdata")
-# SingleCellExperiment with scRNA-seq
-
-# data(MouseDLN.SCE) 
-# # SpatialExperiment with spatial transcriptomics data
-# data(MouseDLN.ST)
-
-# if (!requireNamespace('R.utils', quietly = TRUE)) {
-    
-# }
-
 library('SingleCellExperiment')
 library('SpatialExperiment')
 library("SpatialDDLS")
+library(Seurat)
+
+library(reticulate)
+
+# Function to install TensorFlow if not already installed
+install_tf_if_needed <- function() {
+  # Check if TensorFlow is already available
+  if (!py_module_available("tensorflow")) {
+    message("TensorFlow not found. Installing TensorFlow...")
+    # Install TensorFlow
+    installTFpython(install.conda = TRUE)
+  } else {
+    message("TensorFlow is already installed.")
+  }
+}
+
+install_tf_if_needed()
+
+
+# Function to display versions of Python, TensorFlow, and other packages
+display_versions <- function() {
+  python_version <- py_run_string("import sys; print('Python version:', sys.version)")
+  tensorflow_version <- py_run_string("import tensorflow as tf; print('TensorFlow version:', tf.__version__)")
+  installed_packages <- py_run_string("import pkg_resources; [print(d) for d in pkg_resources.working_set]")
+
+}
+
+display_versions()
+
+
 
 par <- R.utils::commandArgs(trailingOnly=TRUE, asValues=TRUE)
 print(par)
-library(Seurat)
 seurat_obj_scRNA <- readRDS(par$sc_input)
-
-sc_input_sce_obj =  as.SingleCellExperiment(seurat_obj_scRNA )
-
+spatial_seurat_obj <- readRDS(par$sp_input)
 
 
-sc_input_ddls <- createSpatialDDLSobject(
-  sc.data = par$sc_input, 
-  sc.cell.ID.column = "CellID", 
-  sc.gene.ID.column = "GeneSymbol",
-  sc.cell.type.column = "broad_cell_types",
-  st.data = par$sp_input,
-  st.spot.ID.column = "CellID",
-  st.gene.ID.column = "GeneSymbol",
-  sc.filt.genes.cluster = TRUE, 
-  sc.n.genes.per.cluster = 150,
-  sc.min.mean.counts = 2
+coordinates <- matrix(0, nrow = dim(spatial_seurat_obj$counts)[2], ncol = 2)
+counts = spatial_seurat_obj$counts
+
+sp_input_se_obj <- SpatialExperiment::SpatialExperiment(
+    assays = list(counts = as.matrix(spatial_seurat_obj$counts)) ,
+    rowData = data.frame(Gene_ID = spatial_seurat_obj$counts@Dimnames[[1]]),
+    colData = data.frame(Cell_ID = spatial_seurat_obj$counts@Dimnames[[2]]),
+    spatialCoords = coordinates
 )
 
 
-
-set.seed(123)
-sce <- SingleCellExperiment::SingleCellExperiment(
-    assays = list(      
-        counts = matrix(
-            rpois(30, lambda = 5), nrow = 15, ncol = 20,
-                dimnames = list(paste0("Gene", seq(15)), paste0("RHC", seq(20))))
-            ),
+sc_input_sce_obj <- SingleCellExperiment::SingleCellExperiment(
+    assays = list( counts = as.matrix(seurat_obj_scRNA$RNA@counts)),
     colData = data.frame(
-            Cell_ID = paste0("RHC", seq(20)),
-            Cell_Type = sample(x = paste0("CellType", seq(6)), size = 20,
-            replace = TRUE)
+            Cell_ID = seurat_obj_scRNA$RNA@counts@Dimnames[[2]],
+            Cell_Type = seurat_obj_scRNA@meta.data$subclass
     ),
     rowData = data.frame(
-        Gene_ID = paste0("Gene", seq(15))
+        Gene_ID = seurat_obj_scRNA$RNA@counts@Dimnames[[1]]
     )
 )
+
 SDDLS <- createSpatialDDLSobject(
-    sc.data = sce,
+    sc.data = sc_input_sce_obj,
     sc.cell.ID.column = "Cell_ID",
     sc.gene.ID.column = "Gene_ID",
+    st.data = sp_input_se_obj,
+    st.spot.ID.column = "Cell_ID",
+    st.gene.ID.column = "Gene_ID",
+    project = "Simul_example",
     sc.filt.genes.cluster = FALSE
 )
+
+
 SDDLS <- genMixedCellProp(
     object = SDDLS,
     cell.ID.column = "Cell_ID",
@@ -71,71 +81,36 @@ SDDLS <- genMixedCellProp(
     train.freq.spots = 2/3,
     verbose = TRUE
 )
+
 SDDLS <- simMixedProfiles(SDDLS)
-    # training of SDDLS model
-    SDDLS <- trainDeconvModel(object = SDDLS,
-    batch.size = 15,
+
+SDDLS <- trainDeconvModel(object = SDDLS,
+    batch.size = 10,
     num.epochs = 5
 )
-# simulating spatial data
-ngenes <- sample(3:40, size = 1)
-ncells <- sample(10:40, size = 1)
-counts <- matrix(
-rpois(ngenes * ncells, lambda = 5), ncol = ncells,
-    dimnames = list(paste0("Gene", seq(ngenes)), paste0("Spot", seq(ncells)))
-)
-coordinates <- matrix(
-    rep(c(1, 2), ncells), ncol = 2
-)
-st <- SpatialExperiment::SpatialExperiment(
-    assays = list(counts = as.matrix(counts)),
-    rowData = data.frame(Gene_ID = paste0("Gene", seq(ngenes))),
-    colData = data.frame(Cell_ID = paste0("Spot", seq(ncells))),
-    spatialCoords = coordinates
-)
+
+
 SDDLS <- loadSTProfiles(
     object = SDDLS,
-    st.data = st,
+    st.data = sp_input_se_obj,
     st.spot.ID.column = "Cell_ID",
     st.gene.ID.column = "Gene_ID"
 )
-# simplify arguments
-simplify <- list(CellGroup1 = c("CellType1", "CellType2", "CellType4"),
-    CellGroup2 = c("CellType3", "CellType5"))
-SDDLS <- deconvSpatialDDLS(
-    object = SDDLS,
-    index.st = 1,
-    simplify.set = simplify,
-    simplify.majority = simplify
+
+SDDLS_PROPS <- deconvSpatialDDLS(
+  SDDLS, index.st = 1, k.spots = 6, fast.pca = TRUE
 )
-# mouseDLN.SDDLS <- genMixedCellProp(
-#   mouseDLN.SDDLS,
-#   cell.ID.column = "CellID",
-#   cell.type.column = "broad_cell_types",
-#   num.sim.spots = 10000,
-#   n.cells = 50,
-#   min.zero.prop = 5,
-#   balanced.type.cells = TRUE
-# )
 
 
-# mouseDLN.SDDLS <- simMixedProfiles(mouseDLN.SDDLS, threads = 3)
-# mouseDLN.SDDLS <- trainDeconvModel(
-#   mouseDLN.SDDLS,
-#   verbose = FALSE
-# ) 
+deconv_matrix <- SDDLS_PROPS@deconv.spots[[1]][6]$Instrinsic.Proportions
 
+# Remove all spaces and dots from cell names, sort them
+colnames(deconv_matrix) <- stringr::str_replace_all(colnames(deconv_matrix), "[/ .&-]", "")
+deconv_matrix <- deconv_matrix[,sort(colnames(deconv_matrix), method="shell")]
 
+if (nrow(deconv_matrix) != ncol(spatial_seurat_obj$counts)){
+  message("The following rows were removed, possibly due to low number of genes: ",
+          paste0("'", colnames(spatial_seurat_obj$counts)[!colnames(spatial_seurat_obj$counts) %in% rownames(deconv_matrix)], "'", collapse=", "))
+}
 
-
-
-
-# mouseDLN.SDDLS <- deconvSpatialDDLS(
-#   mouseDLN.SDDLS, index.st = 1, k.spots = 6, fast.pca = TRUE
-# )
-# plotSpatialPropAll(mouseDLN.SDDLS, index.st = 1)
-
-
-# remotes::install_version('Matrix', version = '1.6.0')
-# remotes::install_cran(c('SpatialExperiment', 'SingleCellExperiment', 'SummarizedExperiment', 'zinbwave', 'S4Vectors', 'scran', 'scuttle'))
-# remotes::install_github('diegommcc/SpatialDDLS', upgrade='never')
+write.table(deconv_matrix, file=par$output, sep="\t", quote=FALSE, row.names=FALSE)
